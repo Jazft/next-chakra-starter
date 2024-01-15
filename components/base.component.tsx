22e75ade2b50dbedfa8be9312230d0a22ba6a575
NobbyDeveloper <nobby.developer@proton.me>
2024-01-15 17:37:15 +0000

    N/A

--- components/base.component.tsx
+++ components/base.component.tsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 export default () => {
     return (
         <>
-            <Text>Hola Mundo</Text>
-        </>
+            <Text>Welcome to this <b>Chakra UI + Next.js 14 Template!</b></Text>
+        </> 
     );
 }
