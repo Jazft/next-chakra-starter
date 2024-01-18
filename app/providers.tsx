d60b0688975faa6b1f832bc160d85e4075f7c321
NobbyDeveloper <nobby.developer@proton.me>
2024-01-18 22:10:32 +0000

    ✅ Fixed Color Mode

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,28 @@
 // app/providers.tsx
 'use client'
 
-import { fonts } from '@/config/chakra.fonts';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
     config: {
         cssVarPrefix: "chk",
         initialColorMode: "light",
+        useSystemColorMode: false,
     },
     fonts: {
         heading: `'Syne Variable', sans-serif`,
         body: `'Rubik', sans-serif`,
+    },
+    styles: {
+        global: (props) => ({
+            'html, body': {
+                bg: props.colorMode === 'light' ? 'white' : 'neutral.600',
+                lineHeight: 'tall',
+            },
+            a: {
+                color: props.colorMode === 'dark' ? 'teal.300' : 'teal.500',
+            },
+        }),
     },
 });
 
